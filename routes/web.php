66e3c1c5b1a61d9363dbfc7427630d8980f6d2a2
unknown <Daniel-Zarco>
2025-06-05 01:30:14 +0200

    TFG TodoF1

--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,20 @@
 <?php
 
-
-
-use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PilotoController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use App\Models\Usuario;
-
-/*01/06/25*/
-
-use App\Http\Controllers\AuthController;
-use App\Services\F1ApiService;
-
-Route::post('/registro', [AuthController::class, 'registro']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
-
-Route::get('/importar-pilotos', function (F1ApiService $api) {
-    $datos = $api->getCarrerasPorTemporada(2023); // puedes cambiar el año
-    $yaImportados = [];
-
-    foreach ($datos['MRData']['RaceTable']['Races'] as $carrera) {
-        foreach ($carrera['Results'] as $result) {
-            $piloto = $result['Driver'];
-            $id = $piloto['driverId'];
-
-            if (!in_array($id, $yaImportados)) {
-                $api->guardarPiloto(
-                    $piloto['givenName'] . ' ' . $piloto['familyName'],
-                    $piloto['nationality'],
-                    $piloto['dateOfBirth'] ?? null
-                );
-                $yaImportados[] = $id;
-            }
-        }
-    }
-
-    return "Pilotos importados correctamente.";
-});
-/*22/05/25*/
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-//06-05-2025//
-
-Route::get('/ver-pilotos', function () {
-    return view('pilotos');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::get('/pilotos', [PilotoController::class, 'index']);
-
-
-
+require __DIR__.'/auth.php';
